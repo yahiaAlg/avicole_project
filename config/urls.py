c8c88828803781ff67e0550512702adf40e385a4
yahiaAlg <yahialinus21alg@gmail.com>
2026-05-02 08:25:37 +0100

    `Updated config/urls.py to use tuple syntax for urlpatterns`

--- config/urls.py
+++ config/urls.py
@@ -3,15 +3,19 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("core.urls", namespace="core")),
-    path("intrants/", include("intrants.urls", namespace="intrants")),
-    path("stock/", include("stock.urls", namespace="stock")),
-    path("elevage/", include("elevage.urls", namespace="elevage")),
-    path("production/", include("production.urls", namespace="production")),
-    path("achats/", include("achats.urls", namespace="achats")),
-    path("clients/", include("clients.urls", namespace="clients")),
-    path("depenses/", include("depenses.urls", namespace="depenses")),
-    path("reporting/", include("reporting.urls", namespace="reporting")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("", include("core.urls", namespace="core")),
+        path("intrants/", include("intrants.urls", namespace="intrants")),
+        path("stock/", include("stock.urls", namespace="stock")),
+        path("elevage/", include("elevage.urls", namespace="elevage")),
+        path("production/", include("production.urls", namespace="production")),
+        path("achats/", include("achats.urls", namespace="achats")),
+        path("clients/", include("clients.urls", namespace="clients")),
+        path("depenses/", include("depenses.urls", namespace="depenses")),
+        path("reporting/", include("reporting.urls", namespace="reporting")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
