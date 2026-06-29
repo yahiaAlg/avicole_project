206cf551f4c1b5e466fff76d1b7e96d38b7d21b2
yahiaAlg <yahialinus21alg@gmail.com>
2026-06-30 00:15:32 +0100

    feat(settings): update database configuration to use MySQL with environment variables

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,9 @@ Django settings for config project.
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,8 +88,15 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("DB_NAME", "rrcacvmg_avicole_db"),
+        "USER": os.environ.get("DB_USER", "rrcacvmg_admin"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "system2026*"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "3306"),
+        "OPTIONS": {
+            "charset": "utf8mb4",
+        },
     }
 }
 
